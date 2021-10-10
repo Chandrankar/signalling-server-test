423119565c156c0fea998772f8dee961e75c6dce
Chandrankar <chandrankar@hotmail.com>
2021-10-10 15:22:42 +0530

    first commit

---
+++ start_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+server_ip=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | awk 'NR==2 {print
+$0}');
+global_ip=$(python get_global_ip.py)
+echo "global ip is $global_ip"
+echo $server_ip;
+python pythonserver.py $server_ip:$PORT;
+sleep infinity
